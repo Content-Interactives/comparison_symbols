9bc8f4cbbccc2f3ddc6b021497963f4f392862ed
User <user@example.com>
2025-08-09 12:59:18 -0700

    added app

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import ComparisonSymbols from './components/ComparisonSymbols'
+
+
+function App() {
+  return (
+      <div className="App">
+        <ComparisonSymbols />
+      </div>
+  );
+}
+
+
+export default App;
